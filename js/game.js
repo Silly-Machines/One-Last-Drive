e7770faa4c802c0f4125ad0f12826ec22567a56a
viki53 <viki53000@gmail.com>
2015-12-21 15:28:04 +0100

    Revert "Move to Phaser.io"
    This reverts commit f16524afdaf3004f5e9bf1d7966743419a48e846.

--- js/game.js
+++ js/game.js
@@ -1,26 +1,61 @@
-"use strict";
-
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', {
-	preload: preload,
-	create: create,
-	update: update
-});
-
-function preload () {
-	game.load.image('voiture', 'assets/images/tesla-model-x-rear.png');
-}
-
-function create () {
-	game.physics.startSystem(Phaser.Physics.ARCADE);
+(function () {
+	"use strict";
 	
-    var player = game.add.sprite(game.world.width / 2 - 450, game.world.height - 820, 'voiture');
-
-    game.physics.arcade.enable(player);
-
-    // player.animations.add('left', [0, 1, 2, 3], 10, true);
-    // player.animations.add('right', [5, 6, 7, 8], 10, true);
-}
-
-function update () {
+	var G = {
+		canvas: document.getElementById("game-canvas")
+	};
+	G.canvas.width = document.body.clientWidth;
+	G.canvas.height = document.body.clientHeight;
+	G.ctx = G.canvas.getContext("2d");
 	
-}
+	G.setCanvasDimensions = function () {
+		G.w = G.canvas.width;
+		G.w_p = G.w / 100;
+		G.h = G.canvas.height;
+		G.h_p = G.h / 100;
+	}
+	
+	G.setCanvasDimensions();
+	window.addEventListener("resize", G.setCanvasDimensions, false);
+	
+	G.redraw = function () {
+		G.ctx.clearRect(0, 0, G.w, G.h);
+		
+		G.ctx.fillStyle = "red";
+		G.ctx.fillRect(0, 0, G.w, G.h);
+		
+		for (var id in G.tilesets) {
+			G.tilesets[id].drawTile(1, G.ctx);
+		}
+		
+		requestAnimationFrame(G.redraw);
+	}
+	
+	G.tilesets = {};
+	
+	G.registerTileset = function (id, src, tileW, tileH) {
+		G.tilesets[id] = new Tileset(src, tileW, tileH);
+		return G.tilesets[id];
+	}
+	G.getTileset = function (id) {
+		return G.tilesets[id];
+	}
+	
+	G.loadAssets = function (assets) {
+		var loaders = [];
+		
+		assets.forEach(function (asset) {
+			loaders.push(asset.load());
+		})
+		
+		return Promise.all(loaders);
+	}
+	
+	G.loadAssets([
+		G.registerTileset("voiture", "assets/images/tesla-model-x-rear.png")
+	]).then(function () {
+		requestAnimationFrame(G.redraw);
+	});
+	
+	console.dir(G);
+})();
