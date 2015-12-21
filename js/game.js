da73ee368b52598410665523c82ddc40e199c29d
viki53 <viki53000@gmail.com>
2015-12-21 15:28:01 +0100

    Revert "Put code in self-calling function"
    This reverts commit 9a789bff8687d617ae65c94033500f2e31474e9d.

--- js/game.js
+++ js/game.js
@@ -1,28 +1,26 @@
-(function () {
-	"use strict";
+"use strict";
 
-	var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', {
-		preload: preload,
-		create: create,
-		update: update
-	});
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', {
+	preload: preload,
+	create: create,
+	update: update
+});
 
-	function preload () {
-		game.load.image('voiture', 'assets/images/tesla-model-x-rear.png');
-	}
+function preload () {
+	game.load.image('voiture', 'assets/images/tesla-model-x-rear.png');
+}
 
-	function create () {
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-		
-	    var player = game.add.sprite(game.world.width / 2 - 450, game.world.height - 820, 'voiture');
+function create () {
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	
+    var player = game.add.sprite(game.world.width / 2 - 450, game.world.height - 820, 'voiture');
 
-	    game.physics.arcade.enable(player);
+    game.physics.arcade.enable(player);
 
-	    // player.animations.add('left', [0, 1, 2, 3], 10, true);
-	    // player.animations.add('right', [5, 6, 7, 8], 10, true);
-	}
+    // player.animations.add('left', [0, 1, 2, 3], 10, true);
+    // player.animations.add('right', [5, 6, 7, 8], 10, true);
+}
 
-	function update () {
-		
-	}
-})();
+function update () {
+	
+}
